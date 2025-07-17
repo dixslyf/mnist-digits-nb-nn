982955c5aa8a28fad644aca8f2b4e179e5c4ce33
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-07-17 12:48:13 +0800

    style(flake): format

--- flake.nix
+++ flake.nix
@@ -5,58 +5,70 @@
     flake-utils.url = "github:numtide/flake-utils";
     poetry2nix-flake.url = "github:nix-community/poetry2nix";
   };
-  outputs = { nixpkgs, nixpkgs-unstable, flake-utils, poetry2nix-flake, ... }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      unstable-pkgs = nixpkgs-unstable.legacyPackages.${system};
-
-      poetry2nix = poetry2nix-flake.lib.mkPoetry2Nix { inherit pkgs; };
-
-      inherit (poetry2nix) mkPoetryEnv;
-
-      overrides = poetry2nix.overrides.withDefaults
-        (_: prev: {
-          gym-notices = prev.gym-notices.overridePythonAttrs (
-            old: {
-              buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ];
-            }
-          );
-        });
-
-      env = mkPoetryEnv {
-        projectDir = ./.;
-        inherit overrides;
-        preferWheels = true;
-        extraPackages = ps: with ps; [
-          tornado
-
-          python-lsp-server
-          mypy
-          python-lsp-black
-          pyls-isort
-          pylsp-mypy
-          flake8
-          flake8-bugbear
-          rope
-        ];
-      };
-    in
+  outputs =
     {
-      devShell = pkgs.mkShell {
-        packages = [
-          unstable-pkgs.typst
-          pkgs.poetry
-          env
-          pkgs.cudatoolkit
-          pkgs.cudaPackages.cudnn
-        ];
+      nixpkgs,
+      nixpkgs-unstable,
+      flake-utils,
+      poetry2nix-flake,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        unstable-pkgs = nixpkgs-unstable.legacyPackages.${system};
 
-        MPLBACKEND = "WebAgg";
+        poetry2nix = poetry2nix-flake.lib.mkPoetry2Nix { inherit pkgs; };
 
-        # CUDA: https://discourse.nixos.org/t/installing-pytorch-into-a-virtual-python-environment/34720/2
-        LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath (with pkgs; [libz])}:${pkgs.cudatoolkit}/lib:${pkgs.cudaPackages.cudnn}/lib:/run/opengl-driver/lib:$LD_LIBRARY_PATH";
+        inherit (poetry2nix) mkPoetryEnv;
 
-        CUDA_PATH = "${pkgs.cudatoolkit}";
-      };
-    });
+        overrides = poetry2nix.overrides.withDefaults (
+          _: prev: {
+            gym-notices = prev.gym-notices.overridePythonAttrs (old: {
+              buildInputs = (old.buildInputs or [ ]) ++ [ prev.setuptools ];
+            });
+          }
+        );
+
+        env = mkPoetryEnv {
+          projectDir = ./.;
+          inherit overrides;
+          preferWheels = true;
+          extraPackages =
+            ps: with ps; [
+              tornado
+
+              python-lsp-server
+              mypy
+              python-lsp-black
+              pyls-isort
+              pylsp-mypy
+              flake8
+              flake8-bugbear
+              rope
+            ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = [
+            unstable-pkgs.typst
+            pkgs.poetry
+            env
+            pkgs.cudatoolkit
+            pkgs.cudaPackages.cudnn
+          ];
+
+          MPLBACKEND = "WebAgg";
+
+          # CUDA: https://discourse.nixos.org/t/installing-pytorch-into-a-virtual-python-environment/34720/2
+          LD_LIBRARY_PATH = "${
+            pkgs.lib.makeLibraryPath (with pkgs; [ libz ])
+          }:${pkgs.cudatoolkit}/lib:${pkgs.cudaPackages.cudnn}/lib:/run/opengl-driver/lib:$LD_LIBRARY_PATH";
+
+          CUDA_PATH = "${pkgs.cudatoolkit}";
+        };
+      }
+    );
 }
